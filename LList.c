4d41eeb5b37d3b1ef74036f3e86924b0032c886f
HakureiHikaze <wgyaaa123@gmail.com>
2021-09-27 16:40:39 +0800

    dev

---
+++ LList.c
@@ -0,0 +1,61 @@
+//
+// Created by Hikaze on 2021/9/27 15:43.
+// 
+//
+
+#include "LList.h"
+
+void ListAppend(LList* pList, void* pData){
+    if(!pList->pHead){
+        pList->pHead = (LNode*) calloc(1, sizeof (LNode));
+        pList->pHead->pData = pData;
+        return;
+    }
+    LNode * p = pList->pHead;
+    while(p->pNext){
+        p = p->pNext;
+    }
+    p->pNext = (LNode*) calloc(1, sizeof (LNode));
+    p->pNext->pData = pData;
+}
+
+void* ListRemove(LList* pList, size_t index){
+    void * pRtn = 0;
+    LNode * p = pList->pHead;
+    LNode * q = p;
+    for(size_t i = 0; i < index;i++){
+        if(p->pNext){
+            q = p;
+            p = p->pNext;
+        }else{
+            exit(-1);
+        }
+    }
+    if(q == p){
+        pList->pHead = p->pNext;
+        pRtn = p->pData;
+        free(p);
+    }else{
+        q->pNext = p->pNext?p->pNext:0;
+        pRtn = p->pData;
+        free(p);
+    }
+    return pRtn;
+}
+
+size_t ListFindMin(LList list, char(*compareCallback)(void*, void*)){
+    if(!list.pHead) {
+        return 0;
+    }
+    LNode* p = list.pHead;
+    LNode* q = p;
+    size_t index = 0;
+    for(size_t i = 0;p->pNext;i++){
+        p = p->pNext;
+        if(compareCallback(q->pData, p->pData)){
+            q = p;
+            index = i;
+        }
+    }
+    return index;
+}
---
+++ LList.h
@@ -0,0 +1,36 @@
+//
+// Created by Hikaze on 2021/9/27 15:43.
+// 
+//
+
+#pragma once
+#include <stdlib.h>
+
+typedef struct LNode_{
+    void* pData;
+    struct LNode_* pNext;
+}LNode;
+
+typedef struct LList_{
+    LNode* pHead;
+}LList;
+/**
+ * @brief 表尾追加结点
+ * @param pList 链表list
+ * @param pData 欲追加的结点数据项
+ */
+void ListAppend(LList* pList, void* pData);
+/**
+ * @brief 提取表中元素并释放结点空间
+ * @param pList 链表list
+ * @param index 欲删除的结点索引
+ * @return 删除的元素数据项指针
+ */
+void* ListRemove(LList* pList, size_t index);
+/**
+ * @brief 搜索表中当前某数据项最小的元素
+ * @param list 链表list
+ * @param compareCallback 该回调函数判断参数1的某数据项是否大于参数2的某数据项，大于则返回1，否则返回0
+ * @return 列表元素中，某数据项最小的元素的索引
+ */
+size_t ListFindMin(LList list, char(*compareCallback)(void*, void*));
